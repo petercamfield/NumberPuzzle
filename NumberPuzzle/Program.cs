8d1b8767354935123b59993f5f4ada55d25205c7
Peter Camfield <peter@camfield.net>
2011-09-20 10:29:07 +0100

    Created solution

---
+++ NumberPuzzle/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace NumberPuzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ NumberPuzzle/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Number Puzzle")]
+[assembly: AssemblyDescription("Converts numbers to written language form")]
+[assembly: AssemblyCopyright("Copyright © Peter Camfield 2011")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d3acdff3-815e-47b6-86ce-825123ce3a5a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
